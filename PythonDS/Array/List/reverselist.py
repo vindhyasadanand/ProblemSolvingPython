e3c5ed3aa8ac66568adb8ad423423b7ebb9b1922
vindhya Hegde <50870871+vindhyasadanand@users.noreply.github.com>
2020-11-04 09:04:40 +0530

    Reverse a string

---
+++ PythonDS/Array/List/reverselist.py
@@ -0,0 +1,11 @@
+#code
+def reverse(ele):
+    reversed = " "
+    for char in ele:
+        reversed = char + reversed
+    print(reversed)
+        
+tc = int(input())
+for i in range(tc):
+    ele = input()
+    reverse(ele)
